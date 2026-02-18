0a66208769bcdeb7c26085917a6938bfb0e84b30
ykaruus <icarotecno@gmail.com>
2026-02-17 21:24:33 -0400

    refactor(equipment-storage): Refatorado métodos relacionados a atualização e substituição de documentos da entidade Equipment

--- internal/domain/interfaces/equipment_storage.go
+++ internal/domain/interfaces/equipment_storage.go
@@ -3,16 +3,14 @@ package interfaces
 import (
 	"centralService/internal/domain/entities"
 	"context"
-
-	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 type EquipmentStorageInterface interface {
 	InitIndexes() error
 	Create(context context.Context, equipament entities.Equipment) (string, error)
 	GetByID(context context.Context, id string) (*entities.Equipment, error)
-	Put(context context.Context, equipament_id string, update bson.M) error
-	Patch(context context.Context, equipament_id string, update bson.M) error
+	UpdateByFilter(context.Context, *entities.EquipmentFilter)
+	Replace(context.Context, *entities.Equipment)
 	Delete(context context.Context, id string) error
 	List(context context.Context, filter entities.EquipmentFilter) ([]entities.Equipment, error)
 }
